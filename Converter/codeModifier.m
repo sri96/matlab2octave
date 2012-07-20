9a588870aa949e39891e956f3fbc361abe6e5eb3
Adhithya Rajasekaran <adhithyan15@gmail.com>
2012-07-20 14:45:51 -0400

    Added documentation and comments for readability

--- Converter/codeModifier.m
+++ Converter/codeModifier.m
@@ -1,6 +1,56 @@
 function output = codeModifier(input_string_array)
 
-output = ifModifier(input_string_array);
+%codeModifier(input_string_array) is the heart of the matlab2octave
+%converter because it converts the matlab code into octave compatible
+%code.Currently it only has limited capabilities because the conversion
+%process is straight forward from matlab to octave.It converts the code
+%using five subfunctions 
+%
+%   removeTrailingWhiteSpace()
+%   ifModifier()
+%   forModifier()
+%   whileModifier()
+%   switchModifier()
+%
+%
+%The names are self explanatory of what those functions do. They just tweak
+%the corresponding matlab constructs to make them octave compatible. We
+%only identified two major differences that needed to be taken care of
+%while converting from matlab to octave. One is the syntax difference
+%between both languages. The next is octave doesn't support nested
+%functions as of right now. So we are working on adding nested function
+%detection and intimating the user about the presence of it.
+%
+%The philosophy behind writing seperate functions to take care of different
+%things is to enable easy bug fixing when issues arise.
+%
+%Bug Fixes:
+%removeTrailingWhiteSpace function was added as a bug fix to fix
+%inconsistent formatting which caused incompatibility with the produced
+%octave code(Added July 20,2012).
+%
+%elseif bug fix was added to ifModifier() function to fix problems arising
+%from elseif statements present with in matlab's if conditional constructs.
+%(Added July 20,2012)
+%
+%     Copyright (C) 2012  <Adhithya Rajasekaran>
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+output = removeTrailingWhiteSpace(input_string_array);
+
+output = ifModifier(output);
 
 output = forModifier(output);
 
@@ -8,10 +58,16 @@ output = whileModifier(output);
 
 output = switchModifier(output);
 
-%output = outputPreparation(output);
+%The following functions add syntax modifications to the matlab code to
+%make it octave compatible.Please read the octave and matlab documentation
+%to know more about the syntax. 
 
 
 function output = ifModifier(input_array)
+
+%ifModifier(input_array) finds all the 'if' statments and their corresponding
+%'end' statements. Once it finds matching 'end' statements it converts them
+%into 'endif' statements. 
 
 size_of_input = size(input_array);
 
@@ -28,6 +84,8 @@ for x  = 1:size_of_input(1,1)
         
     else
         
+        %Added to fix the elseif bug which was appending multiple ifs to
+        %the same end statements.(Added July 20,2012)
         else_if_locator = strfind(current_row,'elseif');
         
         cell_expansion_2 = else_if_locator{1};
@@ -71,6 +129,10 @@ for x  = 1:size_of_input(1,1)
 end
 
 output = input_array;
+
+%forModifier,whileModifier and switchModifier are reimplementations of
+%ifModifier modified with commands looking and replacing appropriate
+%constructs. 
 
 function output = forModifier(input_array)
 
@@ -231,25 +293,24 @@ end
 
 output = input_array;
 
-% function output = outputPreparation(input_array)
-% 
-% size_of_input = size(input_array);
-% 
-% for x  = 1:size_of_input(1,1)
-%     
-%     current_row = input_array(x,1);
-%     
-%     current_row = strrep(current_row,'%','%%');
-%     
-%     current_row = strrep(current_row,'\','\\');
-%     
-%     current_row = strrep(current_row,'''','''''');
-%     
-%     input_array(x,1) = current_row;
-%     
-% end 
-% 
-% output = input_array;
+%removeTrailingWhiteSpace function is self explanatory. 
+
+function output = removeTrailingWhiteSpace(input_array)
+
+size_of_input = size(input_array);
+
+for x  = 1:size_of_input(1,1)
+    
+    current_row = input_array(x,1);
+    
+    current_row = deblank(current_row);
+    
+    input_array(x,1) = current_row;
+    
+end 
+
+output = input_array;
+
 
 
 
