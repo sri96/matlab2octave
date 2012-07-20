3fe1249fb5b1a43ce6647555ea003c1b8b32f17a
Adhithya Rajasekaran <adhithyan15@gmail.com>
2012-07-20 14:54:38 -0400

    Added documentation and comments

--- Converter/pathExtractor.m
+++ Converter/pathExtractor.m
@@ -1,5 +1,10 @@
 function output = pathExtractor(input_path)
 
+%pathExtractor(input_path) is a reimplementation of functionNameExtractor
+%function in which instead of returning the name of the function from path,
+%this function just returns the remaining string after removing the
+%function name and .m extension. 
+
 m_extension_removal = strfind(input_path,'.m');
 
 remaining_string = input_path(1:m_extension_removal-1);
